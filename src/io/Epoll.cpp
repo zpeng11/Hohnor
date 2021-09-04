414f6ed9e81fd7d300adce8d35e9687a368b8ed8
zpeng11 <zhanfei.peng@ucalgary.ca>
2021-09-04 13:54:46 -0600

    Finished epoll wrap

--- src/io/Epoll.cpp
+++ src/io/Epoll.cpp
@@ -2,15 +2,65 @@
 #include "FdUtils.h"
 #include <sys/epoll.h>
 
-
 using namespace Hohnor;
 
-Epoll::Epoll(int maxEventsSize, bool closeOnExec)
+Epoll::Epoll(size_t maxEventsSize, bool closeOnExec)
+    : events_(std::move(std::unique_ptr<Event[]>(new Event[maxEventsSize]))), maxEventsSize_(maxEventsSize), readyEvents_(0)
 {
-    fd_ = epoll_create1(closeOnExec? EPOLL_CLOEXEC:0);
+    fd_ = epoll_create1(closeOnExec ? EPOLL_CLOEXEC : 0);
 }
 
 Epoll::~Epoll()
 {
     FdUtils::close(fd_);
+}
+
+int Epoll::ctl(int cmd, int fd, Event *event)
+{
+    int ret = epoll_ctl(fd_, cmd, fd, event);
+    if (ret < 0)
+    {
+        LOG_SYSERR << "Hohor::Epoll:ctl() error";
+    }
+    return ret;
+}
+
+int Epoll::add(int fd, int trackEvents, void *ptr)
+{
+    Event e;
+    e.events = trackEvents;
+    if (ptr != NULL)
+        e.data.ptr = ptr;
+    else
+        e.data.fd = fd;
+    return this->ctl(EPOLL_CTL_ADD, fd, &e);
+}
+
+int Epoll::modify(int fd, int trackEvents, void *ptr)
+{
+    Event e;
+    e.events = trackEvents;
+    if (ptr != NULL)
+        e.data.ptr = ptr;
+    else
+        e.data.fd = fd;
+    return this->ctl(EPOLL_CTL_MOD, fd, &e);
+}
+
+int Epoll::remove(int fd)
+{
+    return this->ctl(EPOLL_CTL_DEL, fd, NULL);
+}
+
+int Epoll::wait(int timeout, const sigset_t *sigmask)
+{
+    int ret;
+    if (sigmask == NULL)
+        ret = epoll_wait(fd_, events_.get(), maxEventsSize_, timeout);
+    else
+        ret = epoll_pwait(fd_, events_.get(), maxEventsSize_, timeout, sigmask);
+    if (ret == -1)
+        LOG_SYSERR << "Hohnor::Epoll::wait() ";
+    readyEvents_ = ret;
+    return ret;
 }
--- src/io/Epoll.h
+++ src/io/Epoll.h
@@ -3,30 +3,55 @@
  */
 #pragma once
 #include <cstddef> // NULL
-#include <memory> //std::unique_ptr
+#include <memory>  //std::unique_ptr
+#include <sys/epoll.h>
 #include "NonCopyable.h"
-#include "sys/epoll.h"
-
+#include "Logging.h"
 
 namespace Hohnor
 {
     class Epoll : NonCopyable
     {
     private:
+        typedef struct epoll_event Event;
         int fd_;
         //events for epoll_wait to pass results
-        std::unique_ptr<struct epoll_event[]> events_;
-        //size of events
-        int eventsSize_; 
+        std::unique_ptr<Event[]> events_;
+        //max size of events
+        size_t maxEventsSize_;
+        //ready events after epoll_wait
+        size_t readyEvents_;
+        class Iter
+        {
+        private:
+            Epoll *ptr_;
+            size_t position_;
+
+        public:
+            Iter(Epoll *ptr) : ptr_(ptr), position_(0) {}
+            bool hasNext() { return position_ >= ptr_->readyEvents_; }
+            Event& next()
+            {
+                CHECK(position_ < ptr_->readyEvents_) << " Hohnor::Epoll::Iter::next() error";
+                return ptr_->events_[position_++];
+            }
+            ~Iter() { ptr_->readyEvents_ = 0; }
+        };
 
     public:
-        explicit Epoll(int maxEventsSize = 1024, bool closeOnExec = true);
+        explicit Epoll(size_t maxEventsSize = 1024, bool closeOnExec = true);
+        int fd() { return fd_; }
         //epoll_ctl(2) interface
-        int ctl(int cmd, int fd, struct epoll_event *event);
-        int add(int fd, int events, void *ptr = NULL);
-        int modify(int fd, int events, void *ptr = NULL);
+        int ctl(int cmd, int fd, Event *event);
+        //add fd to the RB tree, by default specify data as fd itself, if ptr is specified, then use the ptr
+        int add(int fd, int trackEvents, void *ptr = NULL);
+        //same as add interface but only modify existing fd in the RB tree
+        int modify(int fd, int trackEvents, void *ptr = NULL);
+        //remove a fd from RB tree
         int remove(int fd);
-        int wait(int timeout);
+
+        int wait(int timeout = -1, const sigset_t *sigmask = NULL);
+        Iter results() { return Iter(this); }
         ~Epoll();
     };
 
